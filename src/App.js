dfee51a45f88d08193f2c56205e505bf089acc0c
Savannaha <savwiley@yahoo.com>
2021-07-21 13:41:13 -0500

    add title & play btn

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Link } from "react-router";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title">Dino-War</div>
+
+      <Link to="/game">Battle!</Link>
     </div>
   );
-}
+};
 
 export default App;
