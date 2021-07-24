abdbf775bd787b26cfb4fb99f3d64552bff0f265
Savannaha <savwiley@yahoo.com>
2021-07-24 11:22:28 -0500

    stylized header & links

--- src/components/Home/styled.js
+++ src/components/Home/styled.js
@@ -5,25 +5,35 @@ export const App = styled.div`
 `;
 
 export const AppHeader = styled.header`
-  background-color: #282c34;
-  min-height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   font-size: calc(10px + 2vmin);
   & p {
-    color: white;
+    display: block;
+    width: 100%;
+    background: var(--blue);
+    color: #fff;
     font-size: calc(10px + 15vmin);
     cursor: default;
     letter-spacing: 2px;
+    padding-top: 30px;
+    text-shadow: 2px 2px 4px rgba(0,0,0,0.8);
+    box-shadow: 2px 2px 4px rgba(0,0,0,0.8);
+    margin-bottom: 25px;
   }
 `;
 
 export const AppLink = styled.a`
-  color: #282c34;
+  color: #fff;
   text-decoration: none;
-  background: rgba(255, 255, 255, 0.3);
-  padding: 5px 10px;
+  text-shadow: 2px 2px 4px rgba(0,0,0,0.3);
+  box-shadow: 2px 2px 4px rgba(0,0,0,0.3);
+  background: var(--red);
+  padding: 10px 10px 5px;
   border-radius: 5px;
   margin-bottom: 15px;
+  & nth-child(1) {
+    color: green;
+  }
 `;
