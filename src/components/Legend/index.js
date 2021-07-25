4623162103fa8eceac324c3099340b29b4483e8a
Tim Jackson <timjacksonm@gmail.com>
2021-07-25 13:43:23 -0500

    Added blue team dino svgs to legend.

--- src/components/Legend/index.js
+++ src/components/Legend/index.js
@@ -3,25 +3,29 @@ import { ReactComponent as OnePointRedDino } from '../../icons/dinosaurs/red-one
 import { ReactComponent as TwoPointsRedDino } from '../../icons/dinosaurs/red-two-points.svg';
 import { ReactComponent as FivePointsRedDino } from '../../icons/dinosaurs/red-five-points.svg';
 import { ReactComponent as TenPointsRedDino } from '../../icons/dinosaurs/red-ten-points.svg';
+import { ReactComponent as OnePointBlueDino } from '../../icons/dinosaurs/blue-one-point.svg';
+import { ReactComponent as TwoPointsBlueDino } from '../../icons/dinosaurs/blue-two-points.svg';
+import { ReactComponent as FivePointsBlueDino } from '../../icons/dinosaurs/blue-five-points.svg';
+import { ReactComponent as TenPointsBlueDino } from '../../icons/dinosaurs/blue-ten-points.svg';
 
 const Legend = ({ color, reversed }) => {
   return (
     <StyledLegend color={color} reversed={reversed}>
       <LegendLabel>Points</LegendLabel>
       <LegendGroup>
-        <Dino as={OnePointRedDino} />
+        <Dino as={color === "var(--red)" ? OnePointRedDino : OnePointBlueDino} />
         <span>1</span>
       </LegendGroup>
       <LegendGroup>
-        <Dino as={TwoPointsRedDino} />
+        <Dino as={color === "var(--red)" ? TwoPointsRedDino : TwoPointsBlueDino} />
         <span>2</span>
       </LegendGroup>
       <LegendGroup>
-        <Dino as={FivePointsRedDino} />
+        <Dino as={color === "var(--red)" ? FivePointsRedDino : FivePointsBlueDino} />
         <span>5</span>
       </LegendGroup>
       <LegendGroup>
-        <Dino as={TenPointsRedDino} />
+        <Dino as={color === "var(--red)" ? TenPointsRedDino : TenPointsBlueDino} />
         <span>10</span>
       </LegendGroup>
     </StyledLegend>
