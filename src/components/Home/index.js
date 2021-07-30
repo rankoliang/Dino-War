9188e8e4c4ddc4d335979b880b451f5958b248c4
Savannaha <savwiley@yahoo.com>
2021-07-30 13:48:44 -0500

    created home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,21 +1,71 @@
-import logo from '../../logo.svg';
-import { App, AppHeader, AppLogo, AppLink } from './styled';
+import { useState, useEffect } from "react";
+import Rules from "../Rules/rules";
+import trianglify from 'trianglify';
+import { App, AppHeader, AppLink, AppIcon, AppExtras } from './styled';
+import { FaArrowCircleLeft, FaCog, FaVolumeUp, FaBook } from "react-icons/fa";
 
 const Home = () => {
+  const [pattern] = useState(
+    trianglify({
+      cellSize: 200,
+      height: 3840,
+      width: 2160,
+      xColors: 'YlGn',
+      colorFunction: trianglify.colorFunctions.shadows(),
+    })
+      .toSVGTree()
+      .toString()
+  );
+
+  useEffect(() => {
+    const ruleBtn = document.querySelector(".rules");
+    const ruleModal = document.querySelector("#modal");
+
+    ruleBtn.addEventListener("click", () => {
+      ruleModal.style.display = "block";
+    })
+  }, [])
+
   return (
-    <App>
+    <App background={pattern}>
       <AppHeader>
-        <AppLogo src={logo} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <Rules />
+        <p className="title">
+          Dino-War
         </p>
         <AppLink
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="#"
+          title="Battle!"
         >
-          Learn React
+          Level 1
         </AppLink>
+        <AppIcon
+          href="#"
+          title="Previous Level"
+        >
+          <FaArrowCircleLeft />
+        </AppIcon>
+
+        <AppExtras>
+          <AppIcon
+            href="#"
+            title="Settings"
+          >
+            <FaCog />
+          </AppIcon>
+          <AppIcon
+            href="#"
+            title="Music"
+          >
+            <FaVolumeUp />
+          </AppIcon>
+          <AppIcon
+            title="Instructions"
+            className="rules"
+          >
+            <FaBook />
+          </AppIcon>
+        </AppExtras>
       </AppHeader>
     </App>
   );
--- src/components/Home/styled.js
+++ src/components/Home/styled.js
@@ -1,40 +1,76 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const App = styled.div`
   text-align: center;
+  height: 100vh;
+  ${({ background }) =>
+    background &&
+    css`
+      background-image: url(\"data:image/svg+xml;utf8,${background}\");
+      background-size: cover;
+      min-height: 100%;
+    `}
 `;
 
 export const AppHeader = styled.header`
-  background-color: #282c34;
-  min-height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
   font-size: calc(10px + 2vmin);
-  color: white;
-`;
-
-export const AppLogo = styled.img`
-  height: 40vmin;
-  pointer-events: none;
-
-  @media (prefers-reduced-motion: no-preference) {
-    & {
-      animation: App-logo-spin infinite 20s linear;
-    }
-  }
-
-  @keyframes App-logo-spin {
-    from {
-      transform: rotate(0deg);
-    }
-    to {
-      transform: rotate(360deg);
-    }
+  background: rgba(255,255,255,0.3);
+  backdrop-filter: blur(4px);
+  height: 100%;
+  & .title {
+    display: block;
+    width: 100%;
+    background: var(--blue);
+    color: #fff;
+    font-size: calc(10px + 15vmin);
+    cursor: default;
+    letter-spacing: 2px;
+    padding-top: 30px;
+    text-shadow: 2px 2px 4px rgba(0,0,0,0.8);
+    box-shadow: 2px 2px 4px rgba(0,0,0,0.8);
+    margin-bottom: 25px;
   }
 `;
 
 export const AppLink = styled.a`
-  color: #61dafb;
+  color: #fff;
+  font-size: calc(10px + 6vmin);
+  text-decoration: none;
+  text-shadow: 2px 2px 2px rgba(0,0,0,0.8);
+  box-shadow: 2px 2px 4px #000;
+  background: var(--red);
+  padding: 14px 14px 0px;
+  border-radius: 5px;
+  margin-bottom: 15px;
+  transition: all 0.2s;
+  &:hover {
+    transform: scale(1.2);
+  }
+`;
+
+export const AppIcon = styled.a`
+  color: var(--red);
+  font-size: calc(10px + 8vmin);
+  filter: drop-shadow(1px 1px 1px #000);
+  fill-rule: #fff;
+  cursor: pointer;
+  transition: all 0.2s;
+  &:hover {
+    transform: scale(1.2);
+  }
+`;
+
+export const AppExtras = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 20%;
+  margin-top: 50px;
+  & ${AppIcon}:nth-child(2) {
+    display: block;
+    position: relative;
+    top: 20px;
+  }
 `;
