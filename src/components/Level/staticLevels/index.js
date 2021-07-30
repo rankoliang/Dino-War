396a7762fbf8ee670e00b7697c2ba36755e68dc5
Tim Jackson <timjacksonm@gmail.com>
2021-07-30 16:40:57 -0500

    changed 5-point-blue.svg to a different dino svg file. Renamed all svgs to correspond with their id in dinos. Re-wrote imports.

--- src/components/Level/staticLevels/index.js
+++ src/components/Level/staticLevels/index.js
@@ -1,26 +1,25 @@
-import { ReactComponent as OnePointRedDino } from '../../../icons/dinosaurs/red-one-point.svg';
-import { ReactComponent as TwoPointsRedDino } from '../../../icons/dinosaurs/red-two-points.svg';
-import { ReactComponent as ThreePointsRedDino } from '../../../icons/dinosaurs/red-three-points.svg';
-import { ReactComponent as FivePointsRedDino } from '../../../icons/dinosaurs/red-five-points.svg';
-import { ReactComponent as TenPointsRedDino } from '../../../icons/dinosaurs/red-ten-points.svg';
-
-import { ReactComponent as OnePointBlueDino } from '../../../icons/dinosaurs/blue-one-point.svg';
-import { ReactComponent as TwoPointsBlueDino } from '../../../icons/dinosaurs/blue-two-points.svg';
-import { ReactComponent as ThreePointsBlueDino } from '../../../icons/dinosaurs/blue-three-points.svg';
-import { ReactComponent as FivePointsBlueDino } from '../../../icons/dinosaurs/blue-five-points.svg';
-import { ReactComponent as TenPointsBlueDino } from '../../../icons/dinosaurs/blue-ten-points.svg';
+import { ReactComponent as Dino01 } from '../../../icons/dinosaurs/dino-01.svg';
+import { ReactComponent as Dino02 } from '../../../icons/dinosaurs/dino-02.svg';
+import { ReactComponent as Dino03 } from '../../../icons/dinosaurs/dino-03.svg';
+import { ReactComponent as Dino04 } from '../../../icons/dinosaurs/dino-04.svg';
+import { ReactComponent as Dino05 } from '../../../icons/dinosaurs/dino-05.svg';
+import { ReactComponent as Dino06 } from '../../../icons/dinosaurs/dino-06.svg';
+import { ReactComponent as Dino07 } from '../../../icons/dinosaurs/dino-07.svg';
+import { ReactComponent as Dino08 } from '../../../icons/dinosaurs/dino-08.svg';
+import { ReactComponent as Dino09 } from '../../../icons/dinosaurs/dino-09.svg';
+import { ReactComponent as Dino10 } from '../../../icons/dinosaurs/dino-10.svg';
 
 export const dinos = {
-    '1': { id: 1, Component: OnePointRedDino },
-    '2': { id: 2, Component: TwoPointsRedDino },
-    '3': { id: 3, Component: ThreePointsRedDino },
-    '4': { id: 4, Component: FivePointsRedDino },
-    '5': { id: 5, Component: TenPointsRedDino },
-    '6': { id: 6, Component: OnePointBlueDino },
-    '7': { id: 7, Component: TwoPointsBlueDino },
-    '8': { id: 8, Component: ThreePointsBlueDino },
-    '9': { id: 9, Component: FivePointsBlueDino },
-    '10': { id: 10, Component: TenPointsBlueDino },
+    '1': { id: 1, Component: Dino01 },
+    '2': { id: 2, Component: Dino02 },
+    '3': { id: 3, Component: Dino03 },
+    '4': { id: 4, Component: Dino04 },
+    '5': { id: 5, Component: Dino05 },
+    '6': { id: 6, Component: Dino06 },
+    '7': { id: 7, Component: Dino07 },
+    '8': { id: 8, Component: Dino08 },
+    '9': { id: 9, Component: Dino09 },
+    '10': { id: 10, Component: Dino10 },
   }
 
   export const legends = {
