b1b1e5d6bfff3512fb21977886c97ae1df99afd2
rankoliang <37024941+rankoliang@users.noreply.github.com>
2021-08-03 17:49:35 -0400

    Revert "Feature: Rules Modal (#19)"
    This reverts commit 50db5d593a2f46d48b1b163fe20161f12189ef69.

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,46 +1,21 @@
-import { useState } from "react";
-import trianglify from 'trianglify';
-import Rules from "../Rules/index";
-import RulesButton from "../Icons/RulesButton";
-import { App, AppHeader, AppLink, AppExtras } from './styled';
+import logo from '../../logo.svg';
+import { App, AppHeader, AppLogo, AppLink } from './styled';
 
 const Home = () => {
-  const [pattern] = useState(
-    trianglify({
-      cellSize: 200,
-      height: 3840,
-      width: 2160,
-      xColors: 'YlGn',
-      colorFunction: trianglify.colorFunctions.shadows(),
-    })
-      .toSVGTree()
-      .toString()
-  );
-  const [ rulesShown, setRulesShown ] = useState(false);
-
   return (
-    <App background={pattern}>
-      {rulesShown && <Rules setShown={setRulesShown} />}
+    <App>
       <AppHeader>
-        <p className="title">
-          Dino-War
+        <AppLogo src={logo} alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <AppLink
-          to="/levels/beginner/1"
-          title="New Game"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          New Game
+          Learn React
         </AppLink>
-        <AppLink
-          to="/levels"
-          title="Level Select"
-        >
-          Level Select
-        </AppLink>
-
-        <AppExtras>
-          <RulesButton setShown={setRulesShown} />
-        </AppExtras>
       </AppHeader>
     </App>
   );
