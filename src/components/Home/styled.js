0c6a9c3ff42bf9dbb7e76a8619af6441383f974a
rankoliang <37024941+rankoliang@users.noreply.github.com>
2021-08-03 17:50:29 -0400

    Revert "Feature: Rules Modal (#19)" (#22)
    This reverts commit 50db5d593a2f46d48b1b163fe20161f12189ef69.

--- src/components/Home/styled.js
+++ src/components/Home/styled.js
@@ -1,61 +1,40 @@
-import styled, { css } from 'styled-components';
-import { Link } from "react-router-dom";
+import styled from 'styled-components';
 
 export const App = styled.div`
   text-align: center;
-  height: 100vh;
-  overflow: hidden;
-  ${({ background }) =>
-    background &&
-    css`
-      background-image: url(\"data:image/svg+xml;utf8,${background}\");
-      background-size: cover;
-      min-height: 100%;
-    `}
 `;
 
 export const AppHeader = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
-  background: rgba(255,255,255,0.3);
-  backdrop-filter: blur(4px);
-  height: 100%;
-  .title {
-    display: block;
-    width: 100%;
-    background: var(--blue);
-    color: #fff;
-    font-size: calc(2em + 13vmin);
-    cursor: default;
-    letter-spacing: 2px;
-    padding-top: 0.3em;
-    text-shadow: 2px 2px 4px rgba(0,0,0,0.8);
-    box-shadow: 2px 2px 4px rgba(0,0,0,0.8);
-    margin-bottom: 0.2em;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+export const AppLogo = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+
+  @media (prefers-reduced-motion: no-preference) {
+    & {
+      animation: App-logo-spin infinite 20s linear;
+    }
+  }
+
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
   }
 `;
 
-export const AppLink = styled(Link)`
-  color: #fff;
-  font-size: calc(1em + 5vmin);
-  text-decoration: none;
-  text-shadow: 2px 2px 2px rgba(0,0,0,0.8);
-  box-shadow: 2px 2px 4px #000;
-  background: var(--red);
-  padding: 0.3em 0.5em 0;
-  border-radius: 5px;
-  margin-bottom: 0.5em;
-  transition: all 0.2s;
-  &:hover {
-    transform: scale(1.2);
-  }
-`;
-
-export const AppExtras = styled.div`
-  display: flex;
-  position: fixed;
-  right: 15px;
-  bottom: 10px;
-  font-size: calc(0.2em + 0.2vmin);
+export const AppLink = styled.a`
+  color: #61dafb;
 `;
